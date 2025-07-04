85dc84b2b7f7aeda80f8f8ef5358c90914e45623
zapirolabs <zapirolabs@gmail.com>
2025-07-05 01:17:04 +0800

    updated favicon

--- landing.fathul.my/bootstrap/ssr/ssr.js
+++ landing.fathul.my/bootstrap/ssr/ssr.js
@@ -3716,7 +3716,7 @@ const _sfc_main$1 = {};
 function _sfc_ssrRender(_ctx, _push, _parent, _attrs) {
   _push(`<div${ssrRenderAttrs(mergeProps({
     class: "flex h-10 w-full items-center justify-center gap-2 md:gap-4 bg-black/90 text-white font-sans animate-gradient-move",
-    style: { "background": "linear-gradient(90deg, #2e3191 0%, #00a551 25%, #2e3191 50%, #00a551 75%, #2e3191 100%), url('https://central.cdn.siza.my/public/zakat2u-v5/assets/images/header-noise.png')", "background-blend-mode": "overlay", "background-repeat": "repeat", "background-size": "200% 100%, 24px 24px" }
+    style: { "background": "linear-gradient(90deg, #2e3191 0%, #00a551 25%, #2e3191 50%, #00a551 75%, #2e3191 100%), url('https://cdn.fathul.my/assets/images/header-noise.png')", "background-blend-mode": "overlay", "background-repeat": "repeat", "background-size": "200% 100%, 24px 24px" }
   }, _attrs))}><p class="font-extrabold text-base leading-[1.4] tracking-[0.01em] flex items-center gap-1">Program Jaminan Kerjaya<span class="opacity-60 font-extrabold ml-1">2025</span></p><a href="/" class="inline-flex items-center justify-center rounded-full bg-white text-black font-extrabold px-3 text-[0.85rem] leading-[1.1] h-6">Daftar</a></div>`);
 }
 const _sfc_setup$1 = _sfc_main$1.setup;
