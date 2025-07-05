5797b0a9befe660663eb5955d5876d85389e9a6a
zapirolabs <zapirolabs@gmail.com>
2025-07-05 17:15:58 +0800

    Error: Ziggy error: route 'home' is not in the route list.

--- reftrack.fathul.my/routes/web.php
+++ reftrack.fathul.my/routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Home\HomeController;
 use App\Http\Controllers\Dashboard\DashboardController;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->name('home');
