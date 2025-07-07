a55e0d88d9a6f3e82efe9676de4924b94a7d5259
zapirolabs <zapirolabs@gmail.com>
2025-07-07 23:07:22 +0800

    update reftrack.fathul.my

--- reftrack.fathul.my/bootstrap/ssr/assets/Dashboard-Bgt8vUyo.js
+++ reftrack.fathul.my/bootstrap/ssr/assets/Dashboard-Bgt8vUyo.js
@@ -317,9 +317,7 @@ const _sfc_main$2 = {
       _push(`<input${ssrRenderAttrs((_temp0 = mergeProps({
         "data-slot": "input",
         class: unref(cn)(
-          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          "focus-visible:border-ring focus-visible:ring-ring focus-visible:ring-[3px]",
-          "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 max-w-xs",
           props.class
         )
       }, _attrs), mergeProps(_temp0, ssrGetDynamicModelProps(_temp0, unref(modelValue)))))}>`);
