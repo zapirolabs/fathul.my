2801bec1d8194b07f920522fced2e50fc33928c0
zapirolabs <zapirolabs@gmail.com>
2025-08-01 13:09:05 +0800

    update [api.fathul.my]

--- api.fathul.my/app/Modules/Notes/routes/api.php
+++ api.fathul.my/app/Modules/Notes/routes/api.php
@@ -4,7 +4,7 @@ use Notes\Http\Controllers\NotesController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('api.auth')->group(function () {
-    Route::prefix('notes')->group(function () {
-        Route::get('/test', [NotesController::class, 'test'])->name('notes.test');
+    Route::prefix('test')->group(function () {
+        Route::get('/', [NotesController::class, 'test'])->name('notes.test');
     });
 });
